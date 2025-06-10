4c460776d126a02c2596806f7a128cb527252187
ZionDere2 <165028736+ZionDere2@users.noreply.github.com>
2025-06-10 14:46:15 +0200

    Update LCFSPR.c

--- vorgabe 10/src/LCFSPR.c
+++ vorgabe 10/src/LCFSPR.c
@@ -1,23 +1,47 @@
-#include "../lib/LCFSPR.h"
+ #include "../lib/LCFSPR.h"
 
 static queue_object* LCFSPR_queue;
-//You can add more global variables here
+-//You can add more global variables here
 
 process* LCFSPR_tick (process* running_process){
-    // TODO
-    return NULL;
+    -    // TODO
+            -    return NULL;
+    +    if(running_process == NULL || running_process->time_left == 0){
+        +        running_process = queue_poll_tail(LCFSPR_queue);
+        +    }
+    +    if(running_process != NULL){
+        +        running_process->time_left--;
+        +    }
+    +    return running_process;
 }
 
 int LCFSPR_startup(){
-    // TODO
-    return 1;
+    -    // TODO
+            -    return 1;
+    +    LCFSPR_queue = new_queue();
+    +    if(LCFSPR_queue == NULL){
+        +        return 1;
+        +    }
+    +    return 0;
 }
 
 process* LCFSPR_new_arrival(process* arriving_process, process* running_process){
-    // TODO
-    return NULL;
+    -    // TODO
+            -    return NULL;
+    +    if(arriving_process != NULL){
+        +        if(running_process != NULL){
+            +            queue_object *node = malloc(sizeof(queue_object));
+            +            node->object = running_process;
+            +            node->next = LCFSPR_queue->next;
+            +            LCFSPR_queue->next = node;
+            +        }
+        +        queue_add(arriving_process, LCFSPR_queue);
+        +        running_process = queue_poll_tail(LCFSPR_queue);
+        +    }
+    +    return running_process;
 }
 
 void LCFSPR_finish(){
-	// TODO
+    -	// TODO
+            +        free_queue(LCFSPR_queue);
 }
