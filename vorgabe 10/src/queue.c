adf92271251aa93f7237ec339dc1626ff4db0acd
ZionDere2 <165028736+ZionDere2@users.noreply.github.com>
2025-06-10 13:33:25 +0200

    Add files via upload

---
+++ vorgabe 10/src/queue.c
@@ -0,0 +1,36 @@
+#include "../lib/queue.h"
+
+int queue_add(void *new_object, queue_object *queue)
+{
+	// TODO
+	return 1;
+}
+
+void *queue_poll(queue_object *queue)
+{
+	// TODO
+	return NULL;
+}
+
+queue_object *new_queue()
+{
+	// TODO
+	return NULL;
+}
+
+void free_queue(queue_object *queue)
+{
+	// TODO
+}
+
+void *queue_peek(queue_object *queue)
+{
+	// TODO
+	return NULL;
+}
+
+void *queue_poll_tail(queue_object *queue) 
+{
+	// TODO
+	return NULL;
+}
